e1a3eac841d1619abfbb7c533c26a1aa3ee84391
Philip <philip_angelin@hotmail.com>
2022-03-19 19:54:40 +0100

    Added 00 to minutes

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { SelectItem } from "../components/Select";
 import { capitalize } from "./string";
 
-const minuteOptions = ["05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60"] as const;
+const minuteOptions = ["00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60"] as const;
 
 export const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"] as const;
 export const minuteSelection: SelectItem[] = minuteOptions.map(option => ({
